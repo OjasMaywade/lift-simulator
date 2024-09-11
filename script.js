5a82f69877b78284027d3b847ae5451f4e6dec4a
Ojas Maywade <ojasmaywade16@gmail.com>
2024-09-11 13:37:40 +0530

    Able to move all the Lift on Button Call

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function createSimulator(){
     }
    }
    //adding lifts
-   for(i=1;i<=numberOfLifts;i++){
+   for(i=0;i<numberOfLifts;i++){
     const createLifts = document.createElement("div")
     createLifts.classList.add("lifts", `lift-${i}`)
     const floor = document.querySelector(".floorLine-0");
@@ -56,20 +56,21 @@ for(i=0;i<=numberOfFloor*2;i++){
         checkAvailability(arr,buttonNum);
         /*  first check the lift nearest to the floor then check the availability and next set the perference if all the lifts are near and available */
         console.log(index)
-        const lift1 = document.querySelector(`.lift-${index}`);
+        const lift1 = document.querySelector(`.lift-${index}`); // Error on this line
+        console.log(lift1)
         const pixel = (buttonNum)*50;
         lift1.dataset.currentFloor = `${buttonNum}`;
         lift1.style.transform = `translateY(-${pixel}px)`; // define transition seconds according to the number of floor gap it has
-        const duration = 2*(Math.abs(lift1.dataset.currentFloor - arr[0].liftCurrentFloor));
+        const duration = 2*(Math.abs(lift1.dataset.currentFloor - arr[index].liftCurrentFloor));
         lift1.style.transitionDuration = `${duration}s`; 
-        arr[0].liftCurrentFloor = lift1.dataset.currentFloor;
-        arr[0].Status = 0;
+        arr[index].liftCurrentFloor = lift1.dataset.currentFloor;
+        arr[index].Status = 0;
         let t = (duration + 5)*1000;
-        //setTimeout(alert("h"),5000)
+        console.log(t)
         setTimeout(()=>{
-            arr[0].Status = 1
+            arr[index].Status = 1
             //console.log(arr[0].Status)
-        }, 5000);
+        }, t);
         // console.log(arr[0].Status)
         //document.getElementById("myBtn").disabled = true;
         
@@ -101,11 +102,10 @@ function checkAvailability(arr,buttonNum){
         //console.log(a.liftCurrentFloor)
         if(a.Status == true){
             nearest.push(Math.abs(a.liftCurrentFloor - buttonNum))
-        }else nearest.push(undefined)
+        }else nearest.push(1000)
         console.log(nearest)
         let minValue = Math.min(...nearest);
         index = nearest.indexOf(minValue);
-            
 })
 }
 
